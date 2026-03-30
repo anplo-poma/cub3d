6396790a9274bc190c7c7a2acdc3adaa80994824
xwang <quentinmozart@gmail.com>
2026-03-30 23:09:21 +0200

    core dump wall fixed

--- srcs/player_move.c
+++ srcs/player_move.c
@@ -6,16 +6,25 @@
 /*   By: xueyan_wang <xueyan_wang@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 23:03:30 by xueyan_wang       #+#    #+#             */
-/*   Updated: 2026/03/28 00:41:44 by xueyan_wang      ###   ########.fr       */
+/*   Updated: 2026/03/30 23:03:21 by xueyan_wang      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3d.h"
 
 
-static int	is_wall(t_game *game, double x, double y)
+static int is_wall(t_game *game, double x, double y)
 {
-	return (game->map.matrix[(int)y][(int)x] == '1');
+    int mx;
+    int my;
+
+    mx = (int)x;
+    my = (int)y;
+    if (mx < 0 || my < 0 || my >= game->map.rows || mx >= game->map.cols)
+        return (1); // 越界一律视为墙
+    if (!game->map.matrix[my])
+        return (1);
+    return (game->map.matrix[my][mx] == '1');
 }
 
 
@@ -26,10 +35,13 @@ static void	move_forward(t_game *game)
 
 	new_x = game->player.player_x + game->player.player_dir_x * MOVESPEED;
 	new_y = game->player.player_y + game->player.player_dir_y * MOVESPEED;
-	if (!is_wall(game, new_x, game->player.player_y))
-		game->player.player_x = new_x;
-	if (!is_wall(game, game->player.player_x, new_y))
-		game->player.player_y = new_y;
+	if (!is_wall(game, new_x + MARGIN, game->player.player_y) &&
+        !is_wall(game, new_x - MARGIN, game->player.player_y))
+        game->player.player_x = new_x;
+
+    if (!is_wall(game, game->player.player_x, new_y + MARGIN) &&
+        !is_wall(game, game->player.player_x, new_y - MARGIN))
+        game->player.player_y = new_y;
 }
 
 static void	move_backward(t_game *game)
@@ -39,10 +51,13 @@ static void	move_backward(t_game *game)
 
 	new_x = game->player.player_x - game->player.player_dir_x * MOVESPEED;
 	new_y = game->player.player_y - game->player.player_dir_y * MOVESPEED;
-	if (!is_wall(game, new_x, game->player.player_y))
-		game->player.player_x = new_x;
-	if (!is_wall(game, game->player.player_x, new_y))
-		game->player.player_y = new_y;
+	if (!is_wall(game, new_x + MARGIN, game->player.player_y) &&
+        !is_wall(game, new_x - MARGIN, game->player.player_y))
+        game->player.player_x = new_x;
+
+    if (!is_wall(game, game->player.player_x, new_y + MARGIN) &&
+        !is_wall(game, game->player.player_x, new_y - MARGIN))
+        game->player.player_y = new_y;
 }
 
 static void	move_left(t_game *game)
@@ -52,10 +67,13 @@ static void	move_left(t_game *game)
 
 	new_x = game->player.player_x - game->player.player_dir_y * MOVESPEED;
 	new_y = game->player.player_y + game->player.player_dir_x * MOVESPEED;
-	if (!is_wall(game, new_x, game->player.player_y))
-		game->player.player_x = new_x;
-	if (!is_wall(game, game->player.player_x, new_y))
-		game->player.player_y = new_y;
+	if (!is_wall(game, new_x + MARGIN, game->player.player_y) &&
+        !is_wall(game, new_x - MARGIN, game->player.player_y))
+        game->player.player_x = new_x;
+
+    if (!is_wall(game, game->player.player_x, new_y + MARGIN) &&
+        !is_wall(game, game->player.player_x, new_y - MARGIN))
+        game->player.player_y = new_y;
 }
 
 static void	move_right(t_game *game)
@@ -65,10 +83,13 @@ static void	move_right(t_game *game)
 
 	new_x = game->player.player_x + game->player.player_dir_y * MOVESPEED;
 	new_y = game->player.player_y - game->player.player_dir_x * MOVESPEED;
-	if (!is_wall(game, new_x, game->player.player_y))
-		game->player.player_x = new_x;
-	if (!is_wall(game, game->player.player_x, new_y))
-		game->player.player_y = new_y;
+	if (!is_wall(game, new_x + MARGIN, game->player.player_y) &&
+        !is_wall(game, new_x - MARGIN, game->player.player_y))
+        game->player.player_x = new_x;
+
+    if (!is_wall(game, game->player.player_x, new_y + MARGIN) &&
+        !is_wall(game, game->player.player_x, new_y - MARGIN))
+        game->player.player_y = new_y;
 }
 
 
